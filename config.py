65716c1379488a353e8b0167b7181ca79c290897
Jaysharma08 <jay366851@gmail.com>
2026-01-21 20:15:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 
+# import os
+
+# BOT_TOKEN = os.getenv("8297419532:AAHN66ZbY-sKff8-1psW66l05ZVYnQ-tBDw")
+# ADMIN_ID = int(os.getenv("5808294584"))
+
 import os
 
-BOT_TOKEN = os.getenv("8297419532:AAHN66ZbY-sKff8-1psW66l05ZVYnQ-tBDw")
-ADMIN_ID = int(os.getenv("5808294584"))
-
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 
